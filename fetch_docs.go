4feeab5ef64d5653d2ede75ff1f5ad1f2a081c90
jackdreilly <jackdreilly@gmail.com>
2017-01-25 17:53:51 -0800

    faking user agent

--- fetch_docs.go
+++ fetch_docs.go
@@ -16,7 +16,19 @@ type DocFetcher interface {
 type standardFetcher int
 
 func (s standardFetcher) Fetch(urlString string) (*goquery.Document, error) {
-	return goquery.NewDocument(urlString)
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", urlString, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return goquery.NewDocumentFromReader(resp.Body)
 }
 
 func (s standardFetcher) Get(urlString string) (*http.Response, error) {
